14fc312dc4d3ccc8ccf7ad0bfe218d791564b0fd
Ilin Dmytry Vladymyrovych <dilin@loe.lviv.ua>
2015-08-20 16:15:44 +0300

    glance open.py

--- open.py
+++ open.py
@@ -1,33 +1,24 @@
 #!/usr/local/bin/python
 #coding=utf-8
-from datetime import datetime
-filyk = 'bill.py'
-sim = []
-try:
-    bill_r = open(filyk, 'r')
-    content = bill_r.read()
-    bill_r.close()
-except IOError:
-    bill_a = open(filyk, 'a')
-    bill_a.write(datetime.strftime(datetime.now(), "%Y%m") + '\n')
-    bill_a.close()
-if content.find(datetime.strftime(datetime.now(), "%Y%m")) == -1:
-        bill_w = open(filyk, 'w')
-        bill_w.write(datetime.strftime(datetime.now(), "%Y%m") + '\n')
-        bill_w.close()
+def check_zero():
+    from datetime import datetime
+    filyk = 'bill.py'
+    sim_example= ['00', '01', '02', '03', '04', '05']
+    try:
+        bill_r = open(filyk, 'r')
+        content = bill_r.read()
+        bill_r.close()
+    except IOError:
+        bill_a = open(filyk, 'a')
+        bill_a.write(datetime.strftime(datetime.now(), "%Y%m") + ' ')
+        bill_a.close()
+        return sim_example
+    if content.find(datetime.strftime(datetime.now(), "%Y%m")) == -1:
+            bill_w = open(filyk, 'w')
+            bill_w.write(datetime.strftime(datetime.now(), "%Y%m") + ' ')
+            bill_w.close()
+            return sim_example
     else:
-        
-
-
-
-
-
-
-'''print len(content)
-stroka = {}
-for i in range(len(content)):
-    stroka[i] = content[i]
-print stroka
-bill_a = open(filyk, 'a')
-bill_a.write('03' + '\n')
-bill_a.close()'''
+        sim = content.split(' ')
+        sim_for_check = [x for x in sim_example if not x in sim]
+        return sim_for_check
